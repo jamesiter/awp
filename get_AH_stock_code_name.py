b49b07735a960fc5d6de3beddaea8fc62ea9d18e
James <James Iter>
2023-07-07 17:30:45 +0800

    加入深圳、上海交易所股票获取

--- get_AH_stock_code_name.py
+++ get_AH_stock_code_name.py
@@ -7,10 +7,20 @@ import akshare as ak
 
 
 del os.environ['ALL_PROXY']
-ahstocks = ak.stock_zh_ah_name()
+# 港股
+# ahstocks = ak.stock_zh_ah_name()
+# 深证证券交易所股票代码和简称
+# ahstocks = ak.stock_info_sz_name_code()
+# 上海证券交易所股票代码和简称
+ahstocks = ak.stock_info_sh_name_code()
+# A 股票
+# ahstocks = ak.stock_info_a_code_name()
 
 for stock in ahstocks.values.tolist():
+    # 港股,上海,A股票
     sql_str = """INSERT INTO stock (name, code, create_time) VALUES ('{name}', '{code}', UNIX_TIMESTAMP(NOW()) * 1000000);""".format(name=stock[1], code=stock[0])
+    # 深圳
+    # sql_str = """INSERT INTO stock (name, code, create_time) VALUES ('{name}', '{code}', UNIX_TIMESTAMP(NOW()) * 1000000);""".format(name=stock[2], code=stock[1])
     print(sql_str)
 # print(ahstocks)
 #
@@ -21,14 +31,14 @@ for stock in ahstocks.values.tolist():
 #
 # print('...')
 
-stock_sh_a_spot_em = ak.stock_sh_a_spot_em()
-stock_sz_a_spot_em = ak.stock_sz_a_spot_em()
-# stock_bj_a_spot_em = ak.stock_bj_a_spot_em()
-H_code_names = ak.stock_hk_spot_em()
-
-# print(H_code_names)
-tencent_bars = ak.stock_hk_hist('00700', period='daily', start_date='19700101', end_date='20230302', adjust='hfq')
-# import pandas as pd
-
-tencent_bars.to_pickle('tencent_day_bars.pkl')
-# print(tencent_bars)
+# stock_sh_a_spot_em = ak.stock_sh_a_spot_em()
+# stock_sz_a_spot_em = ak.stock_sz_a_spot_em()
+# # stock_bj_a_spot_em = ak.stock_bj_a_spot_em()
+# H_code_names = ak.stock_hk_spot_em()
+#
+# # print(H_code_names)
+# tencent_bars = ak.stock_hk_hist('00700', period='daily', start_date='19700101', end_date='20230302', adjust='hfq')
+# # import pandas as pd
+#
+# tencent_bars.to_pickle('tencent_day_bars.pkl')
+# # print(tencent_bars)
